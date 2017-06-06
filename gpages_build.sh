85ed53849cba620fa5f16beeacf4bc0aa678d23c
gpawade <gpawade@tavisca.com>
2017-06-06 09:52:33 +0530

    deplyment issue - tacking

--- gpages_build.sh
+++ gpages_build.sh
@@ -6,17 +6,19 @@ org=$1
 repo=$2
 branch=${3:-"master"} # default to master when branch isn't specified
 
-mkdir temp && cd temp
+mkdir "temp" && cd "temp"
 # make folder (same as input, no checking!)
 #mkdir $repo
 #git clone git@github.com:$org/$repo.git --single-branch
+
 git clone "https://${GH_TOKEN}@${GH_REF}" --single-branch
+
 # switch to gh-pages branch
 #pushd $repo >/dev/null
 git checkout --orphan gh-pages
 
 # remove all content
-git rm -rf -q .
+#git rm -rf -q .
 
 # use bower to install runtime deployment   
 #bower cache clean $repo # ensure we're getting the latest from the desired branch.
